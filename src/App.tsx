1ddf87f93c57e86e87dd5570e241e6ead8cbc9c3
WeylonSantana <weylonsantanaoff@gmail.com>
2024-06-21 12:15:59 -0300

    check for aseprite in path before start and starting exporting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Component } from 'react';
 import { open } from '@tauri-apps/api/dialog';
 import { exists, readTextFile, writeTextFile, BaseDirectory, createDir, readDir, FileEntry } from '@tauri-apps/api/fs';
+import { Command } from '@tauri-apps/api/shell';
+import { dialog, process } from '@tauri-apps/api';
 
 interface AppState {
+  // Config State
   keepConfig: boolean;
-  asepritePath: string;
   fileListPath: string;
+
+  // Local State
   fileList?: FileEntry[];
   selectedFile?: FileEntry;
 }
@@ -15,18 +19,20 @@ export default class App extends Component<any, AppState> {
     super(props);
     this.state = {
       keepConfig: false,
-      asepritePath: '',
       fileListPath: '',
     };
 
+    this.checkForAseprite = this.checkForAseprite.bind(this);
     this.updateConfig = this.updateConfig.bind(this);
-    this.searchAsepritePath = this.searchAsepritePath.bind(this);
     this.searchAsepriteFiles = this.searchAsepriteFiles.bind(this);
     this.loadFileList = this.loadFileList.bind(this);
+    this.handleExport = this.handleExport.bind(this);
   }
 
   async componentDidMount() {
     try {
+      if (!(await this.checkForAseprite())) return;
+
       if (!(await exists('', { dir: BaseDirectory.AppConfig }))) {
         await createDir('', { dir: BaseDirectory.AppConfig });
         await writeTextFile('config.json', JSON.stringify({}), { dir: BaseDirectory.AppConfig });
@@ -35,7 +41,6 @@ export default class App extends Component<any, AppState> {
       var state = JSON.parse(await readTextFile('config.json', { dir: BaseDirectory.AppConfig }));
       this.setState({
         keepConfig: state?.keepConfig ?? false,
-        asepritePath: state?.asepritePath ?? '',
         fileListPath: state?.fileListPath ?? '',
       });
 
@@ -44,6 +49,43 @@ export default class App extends Component<any, AppState> {
       }
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  async checkForAseprite() {
+    try {
+      const command = new Command('Aseprite', ['--version']);
+      await command.spawn();
+      return true;
+    } catch (error) {
+      if (error === 'program not found') {
+        await dialog.message(
+          `Please register Aseprite in your system PATH variable.
+          Step 1: Open Aseprite installation folder.
+          Step 2: Copy the path of the folder.
+          Step 3: Type 'Variables' in the Windows search bar.
+          Step 4: Click on 'Edit the system environment variables'.
+          Step 5: Click on 'Environment Variables' on the end.
+          Step 6: Click on 'Path' on the 'System variables' list.
+          Step 7: Click on 'Edit'.
+          Step 8: Click on 'New'.
+          Step 9: Paste the Aseprite folder path.
+          Must be something like:
+            'C:\\Program Files\\Aseprite'.
+
+          Step 10: Click on 'Ok'.
+          Step 11: Click on 'Ok' again.
+          Step 12: Restart this program and try again.
+          `,
+          { type: 'error', title: 'Aseprite Multiple Export - Aseprite not found!' }
+        );
+
+        await process.exit(1);
+        return false;
+      }
+
+      console.error(error);
+      return false;
     }
   }
 
@@ -56,24 +98,23 @@ export default class App extends Component<any, AppState> {
       }));
 
       if (this.state.keepConfig || (key === 'keepConfig' && value)) {
-        var state = JSON.parse(await readTextFile('config.json', { dir: BaseDirectory.AppConfig }));
-        state[key] = value;
-        await writeTextFile('config.json', JSON.stringify(state, undefined, 2), { dir: BaseDirectory.AppConfig });
+        await writeTextFile(
+          'config.json',
+          JSON.stringify(
+            {
+              keepConfig: this.state.keepConfig,
+              fileListPath: this.state.fileListPath,
+            },
+            undefined,
+            2
+          ),
+          { dir: BaseDirectory.AppConfig }
+        );
       }
 
-      if (!this.state.keepConfig) {
+      if (!this.state.keepConfig || (key === 'keepConfig' && !value)) {
         await writeTextFile('config.json', JSON.stringify({}), { dir: BaseDirectory.AppConfig });
       }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  async searchAsepritePath() {
-    try {
-      var file = await open({ multiple: false, directory: false, filters: [{ name: 'executável', extensions: ['exe'] }] });
-      if (!file || Array.isArray(file)) return;
-      this.updateConfig('asepritePath', file);
     } catch (error) {
       console.error(error);
     }
@@ -100,27 +141,26 @@ export default class App extends Component<any, AppState> {
     }
   }
 
+  async handleExport() {
+    const { selectedFile } = this.state;
+    if (!selectedFile) {
+      await dialog.message('Please select a file to export.', { type: 'error', title: 'Aseprite Multiple Export - No file selected!' });
+      return;
+    }
+  }
+
   render() {
-    const { keepConfig, asepritePath, fileListPath, selectedFile } = this.state;
+    const { keepConfig, fileListPath, selectedFile } = this.state;
 
     return (
       <div>
-        <div className='flex gap-2 mt-6 ml-4 items-center absolute right-4 top-0'>
+        <div className='absolute top-0 flex items-center gap-2 mt-6 ml-4 right-4'>
           <input id='keep-config' type='checkbox' className='checkbox' checked={keepConfig} onChange={() => this.updateConfig('keepConfig', !keepConfig)} />
           <label htmlFor='keep-config'>Keep Changes?</label>
         </div>
 
-        {/* Aseprite Path Search */}
-        <div className='flex gap-2 mt-4 ml-4 items-center'>
-          <label className='label'>Aseprite Path:</label>
-          <input className='input input-sm w-[700px]' type='text' value={asepritePath} disabled />
-          <button className='btn btn-sm btn-success' onClick={this.searchAsepritePath}>
-            Search Aseprite.exe
-          </button>
-        </div>
-
         {/* Aseprite Path Filelist Search */}
-        <div className='flex gap-2 ml-4 items-center'>
+        <div className='flex items-center gap-2 mt-4 ml-4'>
           <label className='label'>Ase Files Path:</label>
           <input className='input input-sm w-[700px]' type='text' value={fileListPath} disabled />
           <button className='btn btn-sm btn-success' onClick={this.searchAsepriteFiles}>
@@ -129,26 +169,33 @@ export default class App extends Component<any, AppState> {
         </div>
 
         {/* Aseprite File List */}
-        <div className='max-w-[800px] max-h-[300px] bg-base-300 rounded-md mt-2 ml-4 overflow-y-scroll'>
-          <table className='table w-full'>
-            <thead>
-              <tr>
-                <th className='text-base'>File Name</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.fileList?.map((file, index) => (
-                <tr key={index} className={`${selectedFile?.name === file.name ? 'bg-base-200' : ''}`}>
-                  <td
-                    className='cursor-pointer select-none'
-                    onClick={() => this.setState({ selectedFile: selectedFile?.name === file.name ? undefined : file })}>
-                    {file.name}
-                  </td>
+        {this.state.fileList?.length && (
+          <div className='max-w-[800px] max-h-[300px] bg-base-300 rounded-md mt-2 ml-4 overflow-y-auto'>
+            <table className='table w-full'>
+              <thead>
+                <tr>
+                  <th className='text-base'>File Name</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {this.state.fileList?.map((file, index) => (
+                  <tr key={index} className={`${selectedFile?.name === file.name ? 'bg-base-200' : ''}`}>
+                    <td
+                      className='cursor-pointer select-none'
+                      onClick={() => this.setState({ selectedFile: selectedFile?.name === file.name ? undefined : file })}>
+                      {file.name}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+
+        {/* Aseprite Button Export */}
+        <button className='absolute btn btn-error bottom-4 right-4' onClick={this.handleExport}>
+          Export!
+        </button>
       </div>
     );
   }
